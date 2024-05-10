6c2397e420041d49d351f548c1d0e75a986f0950
Hasitha Peiris <99813367+it21175152@users.noreply.github.com>
2024-05-10 11:37:34 +0530

    Delete payment-service directory

--- payment-service/src/main/java/com/olp/paymentservice/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.olp.paymentservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.olp.paymentservice.model.Payment;
-
-public interface PaymentRepository extends MongoRepository<Payment, String>{
-
-}
