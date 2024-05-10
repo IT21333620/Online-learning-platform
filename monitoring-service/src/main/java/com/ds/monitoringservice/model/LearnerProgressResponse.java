aa7b18bf71cd7d6dddcc7c144d11846697b3645e
S.A.P Kumarasiri <99177177+IT21365614@users.noreply.github.com>
2024-05-10 16:54:43 +0530

    Get status counts
    Get status count for user id and course id

--- monitoring-service/src/main/java/com/ds/monitoringservice/model/LearnerProgressResponse.java
+++ monitoring-service/src/main/java/com/ds/monitoringservice/model/LearnerProgressResponse.java
@@ -1,27 +1,37 @@
 package com.ds.monitoringservice.model;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class LearnerProgressResponse {
     private List<LearnerProgress> learnerProgressList;
     private int totalCount;
     private long trueCount;
 
-    public LearnerProgressResponse(List<LearnerProgress> learnerProgressList, int totalCount, long trueCount) {
-        this.learnerProgressList = learnerProgressList;
-        this.totalCount = totalCount;
-        this.trueCount = trueCount;
-    }
-
-    public List<LearnerProgress> getLearnerProgressList() {
-        return learnerProgressList;
-    }
-
-    public int getTotalCount() {
-        return totalCount;
-    }
-
-    public long getTrueCount() {
-        return trueCount;
-    }
+//    public LearnerProgressResponse(List<LearnerProgress> learnerProgressList, int totalCount, long trueCount) {
+//        this.learnerProgressList = learnerProgressList;
+//        this.totalCount = totalCount;
+//        this.trueCount = trueCount;
+//    }
+//
+//    public List<LearnerProgress> getLearnerProgressList() {
+//        return learnerProgressList;
+//    }
+//
+//    public int getTotalCount() {
+//        return totalCount;
+//    }
+//
+//    public long getTrueCount() {
+//        return trueCount;
+//    }
 }
